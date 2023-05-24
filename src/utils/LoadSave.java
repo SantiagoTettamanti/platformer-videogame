fa95cd49c2c5ecf3f35c63d1bade4da73ab30c7b
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-05-24 18:52:17 -0500

    Updated GetLevelData method

--- src/utils/LoadSave.java
+++ src/utils/LoadSave.java
@@ -3,6 +3,7 @@ package utils;
 import main.Game;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
@@ -34,6 +35,16 @@ public class LoadSave {
         int[][] lvlData = new int[Game.TILES_IN_HEIGHT][Game.TILES_IN_WIDTH];
         BufferedImage img = GetSpriteAtlas(LEVEL_ONE_DATA);
 
-        for()
+        for(int j = 0; j < img.getHeight(); j++) {
+            for(int i = 0; i < img.getWidth(); i++) {
+                Color color = new Color(img.getRGB(i, j));
+                int value = color.getRed();
+                if(value >= 48) {
+                    value = 0;
+                }
+                lvlData[j][i] = value;
+            }
+        }
+        return lvlData;
     }
 }
