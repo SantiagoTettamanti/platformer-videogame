c6ab781546c8afb6dc9b786e4c4060c0a4dbea24
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2023-07-25 19:46:03 -0500

    Updated Buttons constant

--- src/utilz/Constants.java
+++ src/utilz/Constants.java
@@ -1,14 +1,27 @@
+
 package utilz;
+
+import main.Game;
 
 public class Constants {
 
-    public static class Directions{
+    public static class UI {
+        public static class Buttons {
+            public static final int B_WIDTH_DEFAULT = 140;
+            public static final int B_HEIGHT_DEFAULT = 56;
+            public static final int B_WIDTH = (int) (B_WIDTH_DEFAULT * Game.SCALE);
+            public static final int B_HEIGHT = (int) (B_HEIGHT_DEFAULT * Game.SCALE);
+        }
+    }
+
+    public static class Directions {
         public static final int LEFT = 0;
         public static final int UP = 1;
         public static final int RIGHT = 2;
         public static final int DOWN = 3;
     }
-    public static class PlayerConstants{
+
+    public static class PlayerConstants {
         public static final int IDLE = 0;
         public static final int RUNNING = 1;
         public static final int JUMP = 2;
@@ -20,15 +33,25 @@ public class Constants {
         public static final int ATTACK_JUMP_2 = 8;
 
         public static int GetSpriteAmount(int player_action) {
-            return switch (player_action) {
-                case RUNNING -> 6;
-                case IDLE -> 5;
-                case JUMP, ATTACK_JUMP_1, ATTACK_JUMP_2, ATTACK_1 -> 3;
-                case FALLING -> 1;
-                case GROUND -> 2;
-                case HIT -> 4;
-                default -> 1;
-            };
+            switch (player_action) {
+                case RUNNING:
+                    return 6;
+                case IDLE:
+                    return 5;
+                case HIT:
+                    return 4;
+                case JUMP:
+                case ATTACK_1:
+                case ATTACK_JUMP_1:
+                case ATTACK_JUMP_2:
+                    return 3;
+                case GROUND:
+                    return 2;
+                case FALLING:
+                default:
+                    return 1;
+            }
         }
     }
+
 }
